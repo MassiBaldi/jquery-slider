a3b328125ecc0a766c70e43b4139da6612893054
MassiBaldi <massibaldi85@gmail.com>
2018-11-30 18:19:57 +0100

    settato anche i tasti f j per muoversi tra le foto

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 $('.frecciadx').click(function(){
   //alert('ciao');
+  prossimaFoto();
+});
+
+function prossimaFoto() {
   var imgActive = $('.wrap img.active');
   var palliniActive = $('.pallini i.active');
 
@@ -17,10 +21,14 @@ $('.frecciadx').click(function(){
     $('.wrap img').first().addClass('active');
     $('.pallini i').first().addClass('active');
   }
-});
+}
 
 $('.frecciasx').click(function(){
   //alert('ciao');
+  indietroFoto()
+});
+
+function indietroFoto() {
   var imgActive = $('.wrap img.active');
   imgActive.removeClass('active');
   var palliniActive = $('.pallini i.active');
@@ -36,5 +44,15 @@ $('.frecciasx').click(function(){
     $('.wrap img').last().addClass('active');
     $('.pallini i').last().addClass('active');
   }
+}
 
+//Se premo il tasto f le foto vanno indietro Se invece premo j le foto vanno avanti
+$(document).keypress(function(event) {
+  // console.log(event.which);
+  if (event.which == 102) {
+    prossimaFoto();
+  }
+  else if (event.which == 106) {
+    indietroFoto();
+  }
 });
