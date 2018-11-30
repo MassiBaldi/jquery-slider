d3a0b7d198598de14dcf809c75aa95d8148fa85e
MassiBaldi <massibaldi85@gmail.com>
2018-11-30 18:03:28 +0100

    frecce  pallini foto OK

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
+$('.frecciadx').click(function(){
+  //alert('ciao');
+  var imgActive = $('.wrap img.active');
+  var palliniActive = $('.pallini i.active');
 
+  imgActive.removeClass('active');
+  palliniActive.removeClass('active');
+
+  var nextImg = imgActive.next('img');
+  var nextPallini = palliniActive.next('i');
+
+  if (nextImg.length != 0) {
+    nextImg.addClass('active');
+    nextPallini.addClass('active');
+  }
+  else {
+    $('.wrap img').first().addClass('active');
+    $('.pallini i').first().addClass('active');
+  }
+});
+
+$('.frecciasx').click(function(){
+  //alert('ciao');
+  var imgActive = $('.wrap img.active');
+  imgActive.removeClass('active');
+  var palliniActive = $('.pallini i.active');
+  palliniActive.removeClass('active');
+
+  var prevImg = imgActive.prev('img')
+
+  if (prevImg.length != 0){
+    prevImg.addClass('active');
+    palliniActive.prev('i').addClass('active');
+  }
+  else {
+    $('.wrap img').last().addClass('active');
+    $('.pallini i').last().addClass('active');
+  }
+
+});
